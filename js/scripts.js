52b4387b0c1a20c3e9ea43c86d565d5b8fa77dea
Msayleb <alimmsayleb@gmail.com>
2024-07-31 17:54:49 +0300

    New portfolio

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+/*!
+* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    //  Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+const btn = document.getElementById('submit');
+const successText = document.getElementById('successText');
+
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    btn.value = 'Sending...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_0affowu';
+    btn.disabled = true;
+    emailjs.sendForm(serviceID, templateID, this).then(() => {
+        btn.value = 'Sent!';
+        successText.innerHTML = 'Thank you for reaching out, we will get back to you as soon as possible.';
+    }, (err) => {
+        btn.value = 'Error sending... Please try again';
+    });
+});
